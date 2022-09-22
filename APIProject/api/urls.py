64433696c14e755c3fee9bc73fcf9f9c1ee5ee64
Marcello Graco <97951427+MarGraco@users.noreply.github.com>
2022-09-22 15:52:53 -0300

    Generic APIView & Mixins

---
+++ APIProject/api/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import ArticleList, ArticleDetails 
+
+#article_list, article_details
+
+
+urlpatterns = [
+    
+    path('articles/', ArticleList.as_view()),
+    path('articles/<int:id>/', ArticleDetails.as_view())
+
+    #path('articles/', article_list),
+    #path('articles/<int:pk>/', article_details)
+
+
+]
